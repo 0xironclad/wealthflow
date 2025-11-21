7f1c194c4b25bf2245336227ea01224444190f67
0xcollins <chemerilcollins@gmail.com>
2025-11-21 01:43:09 +0100

    feat: add remote image pattern for Vercel avatars

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,25 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.cache = {
-      type: "filesystem",
-    };
-    return config;
-  },
-  turbopack: {
-    resolveAlias: {
-      "@": path.resolve(__dirname, "src"),
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "avatar.vercel.sh",
+            },
+        ],
+    },
+    webpack: (config) => {
+        config.cache = {
+            type: "filesystem",
+        };
+        return config;
+    },
+    turbopack: {
+        resolveAlias: {
+            "@": path.resolve(__dirname, "src"),
+        }
     }
-  }
 };
 
 export default nextConfig;
