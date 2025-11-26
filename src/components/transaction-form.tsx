3fb90ca3241fa705a0d1e584bc57789a5330cc20
0xcollins <chemerilcollins@gmail.com>
2025-11-26 14:43:01 +0100

    fix: build issues

--- src/components/transaction-form.tsx
+++ src/components/transaction-form.tsx
@@ -5,7 +5,6 @@ import {
     CardTitle,
     CardDescription,
     CardContent,
-    CardFooter,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
